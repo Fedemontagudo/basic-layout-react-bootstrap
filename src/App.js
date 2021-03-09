9122f97f3d0c0fde542cd48291b4d2267e17ca1c
Fede Montagud <fedemontagudo@gmail.com>
2021-03-09 17:18:46 +0100

    commit inicial + initial settings

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+import "./estilos.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <body>
+      <header>
+        <h1 class="titulo">Título</h1>
       </header>
-    </div>
+
+      <main>
+        <article class="contenido">
+          <h2 class="titular">Titular de la noticia</h2>
+          <p class="textos">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Quidem voluptatem voluptatibus corrupti, cum quam aliquid
+            explicabo omnis iusto consectetur, nihil aspernatur,
+            nisi ipsam quae cumque commodi nemo! Nihil, iure error!
+      </p>
+          <p class="textos">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Quidem voluptatem voluptatibus corrupti, cum quam aliquid
+            explicabo omnis iusto consectetur, nihil aspernatur,
+            nisi ipsam quae cumque commodi nemo! Nihil, iure error!
+      </p>
+        </article>
+
+        <aside>
+          <h3 class="titular">Categorías</h3>
+          <ul>
+            <li>Noticias de deporte</li>
+            <li>Noticias de ocio</li>
+            <li>Noticias de política</li>
+            <li>Noticias de espectáculos</li>
+          </ul>
+          <h3 class="titular">Otras cosas</h3>
+          <ul>
+            <li>Nosequé</li>
+            <li>Nosecuanto</li>
+            <li>Talycual</li>
+          </ul>
+        </aside>
+      </main>
+      <footer>
+        <p class="texto-footer">Texto del footer</p>
+      </footer>
+    </body>
+
+
   );
 }
 
